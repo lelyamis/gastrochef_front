458e861a4e2d3613d769810f9d4bf95c3a6e2acf
lelyamis <129838418+lelyamis@users.noreply.github.com>
2023-04-15 15:00:53 +0800

    Update index.jsx

--- components/Articles/index.jsx
+++ components/Articles/index.jsx
@@ -25,7 +25,7 @@ export default function Articles() {
               <div className={styles.articles__info}>
                 <p className={styles.articles__date}>29.05.2020</p>
                 <button className={styles.articles__btn}>
-                  <Link href={{pathname: '/article', query : {article: article.id}}}>Подробнее</Link>
+                  <Link href={{pathname: '/article', query : {article: article.id}}}>Детальніше</Link>
                 </button>
               </div>
             </div>
